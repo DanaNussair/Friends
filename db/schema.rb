b9af8c3bd03612e739f1f590c3c76c88c9d0d0a1
Dana Alnusair <dana.nusair88@gmail.com>
2024-06-08 15:39:48 +0300

    Corrects friends scaffold behavior

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_607_104_059) do
+ActiveRecord::Schema[7.1].define(version: 20_240_608_123_207) do
   create_table 'friends', force: :cascade do |t|
     t.string 'first_name'
     t.string 'last_name'
